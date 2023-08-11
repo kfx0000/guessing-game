2196f904f0a6b7411d8cfd5efd568f0a138ff549
KFX <kfx0000@gmail.com>
2023-08-11 08:17:22 +0300

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let min = 0;
+        let max = 0;
+        let prev = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.prev = Math.ceil((this.min+this.max)/2);
     }
 
     lower() {
-
+        this.max = this.prev;
     }
 
     greater() {
-
+        this.min = this.prev;
     }
 }
 
